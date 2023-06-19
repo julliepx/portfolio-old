e44beed40b4dac41ecc15bda76b864879b5c1ab7
Jullie <jlpaixaof@gmail.com>
2023-06-19 06:30:02 -0300

    Portfolio Inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CVIcon from './components/icons/CVIcon/CVIcon';
+import GitHubIcon from './components/icons/GitHubIcon/GitHubIcon';
+import LinkedInIcon from './components/icons/LinkedInIcon/LinkedInIcon';
+import MailIcon from './components/icons/MailIcon/MailIcon';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="profile-container">
+        <div className="profile-icons">
+          <a href="https://www.linkedin.com/in/jlpaixaof/" target='blank'>
+            <LinkedInIcon />
+          </a>
+          <a href="https://github.com/julliepx" target='blank'>
+            <GitHubIcon />
+          </a>
+          <a href="mailto:jlpaixaof@gmail.com" target='blank'>
+            <MailIcon />
+          </a>
+          <a href="CurriculoJulliePaixao.pdf" target='blank'>
+            <CVIcon />
+          </a>
+        </div>
+        <div className="profile-infos">
+          <h1>Oi, eu sou a Jullie</h1>
+          <h2>Fullstack Developer</h2>
+          <p>Tenho 22 anos e sou desenvolvedora fullstack com 1 ano de experiência trabalhando no mercado.</p>
+        </div>
+        <img src="https://github.com/julliepx.png" alt="Foto de Jullie Paixão, com a língua de fora e usando um headphone preto." />
+      </section>
     </div>
   );
 }
---
+++ src/components/icons/CVIcon/CVIcon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './CVIcon.css';
+
+const CVIcon: React.FC = () => {
+    return <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_15_121)">
+            <path d="M32 0C14.3269 0 0 14.3269 0 32C0 49.6731 14.3269 64 32 64C49.6731 64 64 49.6731 64 32C64 14.3269 49.6731 0 32 0ZM26.4922 15.4023H28.6094V23.4844H20.2383V21.4414L26.4922 15.4023ZM31.4492 15.4023H43.7617V48.5977H20.2383V26.2266H31.4492V15.4023Z" fill="#EA4476" />
+        </g>
+        <defs>
+            <clipPath id="clip0_15_121">
+                <rect width="64" height="64" fill="white" />
+            </clipPath>
+        </defs>
+    </svg>
+        ;
+}
+
+export default CVIcon;
