99ba42a3c2d759e9b6c85989b0efbef03a656756
Jullie <jlpaixaof@gmail.com>
2023-06-21 04:50:25 -0300

    Ajustes em dispositivos menores que 380px

--- src/components/icons/About/About.tsx
+++ src/components/icons/About/About.tsx
@@ -6,7 +6,7 @@ import ArrowDownIcon from '../ArrowDownIcon/ArrowDownIcon'
 const About = () => {
   return (
     <div className="about-infos">
-        <p>Olá! Sou uma entusiasta da engenharia de software com uma paixão especial por qualidade. Com uma sólida formação em desenvolvimento de software, adquiri experiência trabalhando com diversas tecnologias, incluindo Java, Spring Framework, Typescript, Angular e, mais recentemente, React. Essa experiência me permitiu ter uma visão abrangente e integrada dos projetos em que estou envolvida, desde o design até a entrega final.</p>
+        <p>Sou uma entusiasta da engenharia de software com uma paixão especial por qualidade. Com uma sólida formação em desenvolvimento de software, adquiri experiência trabalhando com diversas tecnologias, incluindo Java, Spring Framework, Typescript, Angular e, mais recentemente, React. Essa experiência me permitiu ter uma visão abrangente e integrada dos projetos em que estou envolvida, desde o design até a entrega final.</p>
         <div className="back-button">
             <Link to="/"><ArrowDownIcon /></Link>
         </div>
